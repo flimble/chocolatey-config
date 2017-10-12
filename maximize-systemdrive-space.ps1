7842443fe9861ad186a78f9373994b8221a39fa0
John Fattal <john.fattal@foxycode.com>
2017-10-12 10:37:46 +0100

    Update maximize-systemdrive-space.ps1

--- maximize-systemdrive-space.ps1
+++ maximize-systemdrive-space.ps1
@@ -4,3 +4,5 @@ $temp = "D:\Temp"
 New-Item -ItemType directory -Path $temp -force
 $env:temp = $temp
 $env:tmp = $temp
+
+set-itemproperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyEnable -value 0 
